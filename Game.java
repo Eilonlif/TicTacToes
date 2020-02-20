cbd1c046c3a59e1bce44969d51c2dfee20eaab02
Eilonlif <43208668+Eilonlif@users.noreply.github.com>
2020-02-20 17:43:01 +0200

    Update Game.java

--- Game.java
+++ Game.java
@@ -3,12 +3,9 @@ package idanTicTacToes;
 import java.util.*;
 public class Game {
 	public static Scanner in = new Scanner(System.in);
-
 	public Board b = new Board(3);
 	public Player p1 = new Player("O");
 	public Player p2 = new Player("X");
-	
-	
 	public void play(){
 		b.create();
 		int whoTurn = 1;  // 1: X, 0: O.
@@ -16,7 +13,6 @@ public class Game {
 		for(int i = 0; i < b.size * b.size; i++){
 			if(whoTurn == 1){ whoTurn = 0; }
 			else{ whoTurn = 1; }
-
 			b = playerAry[whoTurn].turn(b);
 			b.print();
 
